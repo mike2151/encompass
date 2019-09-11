ba08e9e0e7d41aa17ee7296413a3e67320ebecbc
Mike Abelar <redefiningproductions@gmail.com>
2019-09-10 22:27:14 -0400

    frontend multiple file support

--- interview_q_instance/views.py
+++ interview_q_instance/views.py
@@ -7,6 +7,9 @@ from .util import create_submission
 from compile.util.runner import run_code
 from django.http import HttpResponseRedirect
 from submission_result.models import SubmissionResult
+from starter_code.models import StarterCode
+import json
+
 
 class AllQuestionsToAnswerView(View):
     template_name = "interview_q_instance/browse.html"
@@ -20,12 +23,26 @@ class QuestionAnswerView(View):
     def get(self, request, *args, **kwargs):
         question = InterviewQuestionInstance.objects.get(pk=self.kwargs.get('pk'))
         api_methods = MethodSignature.objects.filter(interview_question_api=question.base_question.api)
-        example_code_snippets = ExampleCode.objects.filter(interview_question=question.base_question)
+        files_to_work_on = StarterCode.objects.filter(interview_question=question.base_question)
+
+        files_to_work_on_names = []
+        files_to_work_on_bodies = []
+        for file_obj in files_to_work_on:
+            filename = file_obj.code_file.name.split("/")[-1]
+            files_to_work_on_names.append(filename)
+            f = file_obj.code_file
+            f.open(mode='r') 
+            content = f.readlines()
+            f.close()
+            files_to_work_on_bodies.append(content)
+
 
         return render(request, self.template_name, {
             'question': question,
             'api_methods': api_methods,
-            'example_code_snippets': example_code_snippets
+            'example_code_snippets': [],
+            'files_to_work_on_bodies': json.dumps(files_to_work_on_bodies),
+            'files_to_work_on_names': files_to_work_on_names
             })
 
     def post(self, request, *args, **kwargs):
