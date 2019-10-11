7c7fab8fa92ebddb88a6e7d1f7a34a38576c9169
root <redefininproductions@gmail.com>
2019-10-11 12:28:35 +0000

    update docker config

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
+from django.conf import settings
 # ----------------------  BASE PARAMETERS ---------------------- #
-DOCKER_IMAGE = 'mike/compiler'
-LOCAL_DIR = '/Users/mike/Desktop/encompassinterviews/encompassinterviews/temp'
-
+DOCKER_IMAGE = 'compiler'
+if settings.DEBUG:
+    LOCAL_DIR = '/Users/mike/Desktop/encompassinterviews/encompassinterviews/temp'
+    CONTAINER_DIR = '/data'
+else:
+    LOCAL_DIR = '/home/django/encompass/temp'
+    CONTAINER_DIR = '/home/django/data'
 # ----------------------  ADDITIONAL PARAMETERS ---------------------- #
 MEMORY_LIMIT = '8000k'  
 AUTO_REMOVE = True 
 FILE_OPEN_MODE = 'ro' 
-CONTAINER_DIR = '/data'
 CONTAINER_TIMEOUT = 10 
 NETWORK_DISABLED = True
